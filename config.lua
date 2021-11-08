620c78c77ad25c68f50e03421f50dd57d9d579f0
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-11-08 13:07:36 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config.DefaultVolume = 0.1 -- Accepted values are 0.01 - 1
 
 Config.Locations = {
     ['vanilla'] = {
-        ['job'] = 'ambulance', -- Required job to use booth
+        ['job'] = 'vanilla', -- Required job to use booth
         ['radius'] = 30, -- The radius of the sound from the booth
         ['coords'] = vector3(120.52, -1281.5, 29.48), -- Where the booth is located
         ['playing'] = false
